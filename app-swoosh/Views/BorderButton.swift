bbbf33a1c8cfb7dadc9d3ed48b4c1c9a5e7b67e3
Ruhullah Rahimov <rahimov@Ruhullah-MacBook-Pro.local>
2020-12-28 22:17:17 +0400

    first commit

---
+++ app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,18 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Ruhullah Rahimov on 28.12.20.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
