177652c57102fcf2889f463a5a5504b5e863b93a
Ruhullah Rahimov <rahimov@Ruhullah-MacBook-Pro.local>
2020-12-31 08:48:27 +0400

    segues and unwind

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
