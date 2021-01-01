0ea471489fa9b2e7bd63db6962ac94e7c5976cbd
Ruhullah Rahimov <rahimov@Ruhullah-MacBook-Pro.local>
2021-01-01 08:14:52 +0400

    IBActions

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ruhullah Rahimov on 31.12.20.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var skillLevel: String?
+    //Niye ?optional olmalidir?
+}
